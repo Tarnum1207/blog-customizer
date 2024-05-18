bb7e83eac6f73c7d70500a4e4124df0d8b15887b
Tarnum1207 <muraskin182@mail.ru>
2024-05-18 17:56:42 +0300

    blog-customizer

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,27 @@
 import arrow from 'src/images/arrow.svg';
-
 import styles from './ArrowButton.module.scss';
+import clsx from 'clsx';
 
-/** Функция для обработки открытия/закрытия формы */
-export type OnClick = () => void;
+export type ArrowButtonProps = {
+	isActive: boolean;
+	onClick: () => void;
+};
 
-export const ArrowButton = () => {
+export const ArrowButton = (props: ArrowButtonProps) => {
+	const { onClick, isActive } = props;
+
 	return (
-		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
+			onClick={() => onClick()}
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(styles.container, isActive && styles.container_open)}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx(styles.arrow, isActive && styles.arrow_open)}
+			/>
 		</div>
 	);
 };
--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,10 +1,6 @@
 declare module '*.svg' {
-	import React = require('react');
-	export const ReactComponent: React.FunctionComponent<
-		React.SVGProps<SVGSVGElement>
-	>;
-	const src: string;
-	export default src;
+	const content: string;
+	export default content;
 }
 
 declare module '*.png';
